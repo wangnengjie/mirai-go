38c46093281699608d7d4bf4d978e5b8552715e3
wangnengjie <751614701@qq.com>
2020-08-21 20:57:39 +0800

    refactor: modify examples

--- example/sendimage.go
+++ example/sendimage.go
@@ -3,18 +3,27 @@ package main
 import (
 	"github.com/wangnengjie/mirai-go"
 	"github.com/wangnengjie/mirai-go/model"
-	"net/url"
 )
 
 func main() {
-	c := mirai.NewClient("client1", url.URL{Scheme: "http", Host: "127.0.0.1:8080"}, "12345678")
-	b := c.AddBot(123456789, true, 0)
-	b.On(model.FriendMessage, returnImg)
-	c.Listen(true)
+	bot := mirai.NewBot(mirai.BotConfig{
+		Host:      "127.0.0.1:8080",
+		AuthKey:   "12345678",
+		Id:        123456789,
+		Websocket: true,
+		RecvMode:  mirai.RecvAll,
+		Debug:     true,
+	})
+	err := bot.Connect()
+	if err != nil {
+		bot.Log.Error(err)
+	}
+	bot.On(model.FriendMessage, returnImg)
+	bot.Loop()
 }
 
-func returnImg(b *mirai.Bot, msg model.MsgRecv) {
-	m := msg.(*model.FriendMsg)
+func returnImg(ctx *mirai.Context) {
+	m := ctx.Message.(*model.FriendMsg)
 	path := ""
 	for _, single := range m.MessageChain {
 		switch single.GetType() {
@@ -25,8 +34,8 @@ func returnImg(b *mirai.Bot, msg model.MsgRecv) {
 	mc := model.NewMsgChainBuilder().Image("url", path).Done()
 	// mc := model.NewMsgChainBuilder().Image("imageId", path).Done()
 	// mc := model.NewMsgChainBuilder().Image("path", path).Done() ; Relative to "plugins/MiraiAPIHTTP/images"
-	_, err := b.SendFriendMessage(m.Sender.Id, mc, 0)
+	_, err := ctx.Bot.SendFriendMessage(m.Sender.Id, mc, 0)
 	if err != nil {
-		b.Log.Errorln(err)
+		ctx.Bot.Log.Error(err)
 	}
 }
